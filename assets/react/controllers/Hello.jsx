301d535f9170fe3aa72efa9502430b0395cb480c
Taymour-NRJ <tnegib@externe.nrjgroup.fr>
2024-10-18 16:52:08 +0200

    Put React component state into html element

--- assets/react/controllers/Hello.jsx
+++ assets/react/controllers/Hello.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function Hello(props) {
-    const [count, setCount] = useState(0);
-
     const incrementCount = () => {
-        setCount(count + 1);
+        const countElement = document.getElementById('count');
+        let currentCount = parseInt(countElement.textContent, 10);
+        currentCount += 1;
+        countElement.textContent = currentCount;
     };
 
     return (
         <div>
             <h1>Hello, {props.fullName}!</h1>
-            <p>You have clicked {count} times.</p>
+            <p>You have clicked <span id="count">0</span> times.</p>
             <button onClick={incrementCount}>Click me!</button>
         </div>
     );
